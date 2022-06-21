91d3a1d8c6854530dcce6c515a1082c967d5be5c
Timaty <101876583+T1maty@users.noreply.github.com>
2022-06-21 13:02:13 +0300

    Add project files.

---
+++ Domain/Note.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Note
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? EditDate { get; set; }
+    }
+}
---
+++ NotesApplication/Interfaces/INotesDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace NotesApplication.Interfaces
+{
+    public interface INotesDbContext
+    {
+        DbSet<Note> Notes { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
